697bd1410344d791b845e48547c4b8a193134c0d
vivid344 <b1014127@fun.ac.jp>
2018-02-10 22:36:22 +0900

    ちょい変更

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (user && pass) {
 
 app.use(express.logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname + '/public/html'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
